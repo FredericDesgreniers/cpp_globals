ade6063d80149adc08fad9884a7b8f4badc80807
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2018-12-17 11:21:29 -0500

    Update and rename globals.h to globals.hpp

---
+++ globals.hpp
@@ -0,0 +1,26 @@
+namespace globs {
+    template <typename T, size_t max>
+    T temps[max];
+
+    /// Typesafe, runtime error free global temporary variables.
+    template <size_t N, typename T = int, size_t max = 20>
+    class temp {
+        static_assert(N < max, "Temp out of range"); // 100% garanteed to prevent runtime errors!
+
+    public:
+        // Gotta make this a template to preserve perfect moving l-values
+        /// Assign to global temp
+        template <typename U>
+        U& operator=(U&& rhs) {
+            auto& temp = temps<std::remove_reference_t<U>, max>[N];
+            temp = std::forward<U>(rhs);
+
+            return temp;
+        }
+
+        /// Get writable reference to global temp
+        T& operator*() {
+            return temps<T, max>[N];
+        }
+    };
+}
